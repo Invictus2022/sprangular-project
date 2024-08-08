c0cc64a221f238fa7f71ab19a19310e91c41c532
hritik <rohan.kinshu@gmail.com>
2024-08-08 18:22:27 +0530

    started

---
+++ sprangular/src/main/java/com/sprangular/sprangular/SprangularApplication.java
@@ -0,0 +1,13 @@
+package com.sprangular.sprangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprangularApplication.class, args);
+	}
+
+}
---
+++ sprangular/src/test/java/com/sprangular/sprangular/SprangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprangular.sprangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
